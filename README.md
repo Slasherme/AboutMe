9ed36c13ec8810d3363335fd477bcfd74fc38b53
aalbarqi <ashjanalbarqi2@gmail.com>
2020-06-09 15:05:00 +0300

    lab03

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # AboutMe
-About Me guessing game
+About Me is a guessing game, the website asks the user some questions about me, and the user answers with what S/He knows.
--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 
 'use strict';
+var correct=0, attempt = 0, attempt2=0;
 var userName =  prompt('What is your name?');
 var iceCream = prompt('Welcome '+ userName +'! Let me ask you some questions, Do you think I like ice-cream?(Y/N)').toUpperCase();
 
 if(iceCream === 'Y'){
   alert('You\'re right, I could spend my whole life eating only ice-cream');
+  correct++;
   //console.log("You're right, I could spend my whole life eating only ice-cream");
 } else if (iceCream === 'N') {
   alert('Are you serious? who does\'nt like ice cream!!!');
@@ -18,6 +20,7 @@ var travel = prompt('Do I like to travel?(Y/N)').toUpperCase();
 
 if(travel === 'Y'){
   alert('right, I love travelling');
+  correct++;
   //console.log("right, I love travelling");
 } else if (travel === 'N') {
   alert('Well, you don\'t know me then');
@@ -31,6 +34,7 @@ var city = prompt('Do you think I live in a famous city?(Y/N)').toUpperCase();
 
 if(city === 'Y'){
   alert('Correct!, I live in Gaza, which is always on the news');
+  correct++;
   //console.log("Correct!, I live in Gaza, which is always on the news");
 } else if (city === 'N') {
   alert('Think again buddy');
@@ -44,6 +48,7 @@ var study = prompt('Have I ever been to college?(Y/N)').toUpperCase();
 
 if(study === 'Y'){
   alert('Yes, I hold a bachelor\'s degree in Engineering');
+  correct++;
   //console.log("Yes, I hold a bachelor's degree in Engineering");
 } else if (study === 'N') {
   alert('Why would you assume that?!!');
@@ -57,6 +62,7 @@ var rich = prompt('I am a millionaire, do you think I\'m lying?(Y/N)').toUpperCa
 
 if(rich === 'Y'){
   alert('right, maybe one day');
+  correct++;
   //console.log("right, maybe one day");
 } else if (rich === 'N') {
   alert('lol, do you need some money?');
@@ -66,4 +72,44 @@ if(rich === 'Y'){
   //console.log('You should answer with Y or N');
 }
 
-alert('Well Done '+ userName);
+var luckyNo = Math.floor(Math.random() * 100);
+
+do{
+  var lucky =  prompt('What is my lucky number?(Answer with a number)');
+  var guess= parseInt(lucky);
+  if(guess > luckyNo){
+    alert('Too high, Try again!');
+  } else if(guess < luckyNo) {
+    alert('Too low, Try again!');
+  } else if(guess === luckyNo){
+    alert('Correct');
+    correct++;
+  }
+  attempt++;
+} while(attempt<4);
+
+if(attempt === 4){
+  alert('Actually, my lucky number is '+ luckyNo);
+}
+
+var arr = ['sherlock','hannibal','prison break','ordeal by innocence',
+  'breaking bad','game of thrones','the last kingdom','house','the mentalist','modern family'];
+do{
+  var shows =  prompt('Guess one of my favorite TV shows.').toLowerCase();
+  for(var i=0; i < arr.length; i++){
+    if(shows === arr[i]){
+      attempt2=6;
+      correct++;
+      alert('Great, That\'s one of them');
+      alert('My favorite TV shows are '+ arr);
+      break;
+    }
+  }
+  attempt2++;
+} while(attempt2<6);
+
+if(attempt2===6){
+  alert('My favorite TV shows are '+ arr);
+}
+
+alert('Well Done '+ userName + ' You have answered ' + correct + ' Questions correctly!');
